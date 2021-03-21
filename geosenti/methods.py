de2bb0f66d309e13331f3ee59ccb16dc41358efb
Dylan Mancero <israelman@hotmail.co.uk>
2021-03-21 21:30:54 +0000

    Removed code that tested for correct length of tweets to be analysed. Added code to construct a dict of results to be sent back

--- geosenti/methods.py
+++ geosenti/methods.py
@@ -21,28 +21,46 @@ auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 api = tweepy.API(auth)
 
 def starter(input):
-    britishTweets = tweepy.Cursor(api.search, q=str(input),geocode = UK_GEO, lang = 'en',tweet_mode='extended').items(10)
-    usaTweets = tweepy.Cursor(api.search, q=str(input),geocode = USA_GEO, lang = 'en',tweet_mode='extended').items(10)
+    britishTweets = tweepy.Cursor(api.search, q=str(input),geocode = UK_GEO, lang = 'en',tweet_mode='extended').items(5)
+    usaTweets = tweepy.Cursor(api.search, q=str(input),geocode = USA_GEO, lang = 'en',tweet_mode='extended').items(5)
  
     britishTweets = list(map(FullTextHandler, britishTweets))
     usaTweets = list(map(FullTextHandler, usaTweets))
     
-    britishSentiment = list(map(lambda tweet: str(analyzer.polarity_scores(tweet)), britishTweets))
-    usaSentiment = list(map(lambda tweet: str(analyzer.polarity_scores(tweet)), usaTweets))
+    britishSentiment = list(map(lambda tweet: analyzer.polarity_scores(tweet), britishTweets))
+    usaSentiment = list(map(lambda tweet: analyzer.polarity_scores(tweet), usaTweets))
 
-    f = open("tests.txt", "w", encoding="utf-8")
-    count = 0
-    for Tweet,Senti in zip_longest(britishTweets, britishSentiment):
-        f.write(str(count) + " " + Tweet + "-->" + Senti + "\n")
-        count += 1
+    britishScore = {'pos':0, 'neg':0, 'neutral':0}
+    usaScore = {'pos':0, 'neg':0, 'neutral':0}
 
-    f.write("\n\nUSA.............\n\n")
-    count = 0
-
-    for Tweet,Senti in zip_longest(usaTweets, usaSentiment):
-        f.write(str(count) + " " + Tweet + "-->" + Senti + "\n")
-        count += 1
- 
+    for score in britishSentiment:
+        compound = score['compound']
+        if compound >= 0.05:
+            current = britishScore['pos']
+            britishScore['pos'] = current + 1
+        elif compound <= -0.05:
+            current = britishScore['neg']
+            britishScore['neg'] = current + 1
+        else:
+            current = britishScore['neutral']
+            britishScore['neutral'] = current + 1
+            
+    for score in usaSentiment:
+        compound = score['compound']
+        if compound >= 0.05:
+            current = usaScore['pos']
+            usaScore['pos'] = current + 1
+        elif compound <= -0.05:
+            current = usaScore['neg']
+            usaScore['neg'] = current + 1
+        else:
+            current = usaScore['neutral']
+            usaScore['neutral'] = current + 1
+    f = open('scoreTest.txt', 'w', encoding='utf-8')
+    for score in britishSentiment:
+        f.write(str(score) + "\n")
+    f.write(str(britishScore))
+    f.close
 def FullTextHandler(Tweet):
     try:
         return str(Tweet.retweeted_status.full_text)
